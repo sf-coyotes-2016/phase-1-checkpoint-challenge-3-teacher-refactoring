efa2610c2c873bca8abe1981ca55a609c6c2b398
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-10-13 09:45:31 -0700

    Add shared methods of Educator children classes to Educator

--- educator.rb
+++ educator.rb
@@ -1,13 +1,29 @@
 class Educator 
-  attr_reader :age, :phase
+  attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
-  def initialize 
+  def initialize(options = {})
     @phase = 3 
     @age = options.fetch(:age, 0)
     @name = options.fetch(:name, "")
   end
 
-  
+  def offer_high_five
+    "High five!"
+  end
+
+  def set_phase(num)
+    @phase = num
+    "Cool, I've always wanted to teach phase #{num}!"
+  end
+
+  def salary=(new_salary)
+    puts "This better be good!"
+    @salary = new_salary
+  end
+
+  def receive_raise(raise)
+    @salary += raise
+  end
 
 end
