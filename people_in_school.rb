4221c47d1d11f441bdbe37a8ad7e48f2fb299568
CatonNip <vivinguyen000@gmail.com>
2016-10-13 10:06:59 -0700

    Dry up classes with common methods and attributes

--- people_in_school.rb
+++ people_in_school.rb
@@ -1,18 +1,14 @@
-require_relative 'student'
-require_relative 'apprentice_teacher'
-require_relative 'senior_teacher'
-
 class PeopleInSchool
-	 attr_reader :age, :phase
+	attr_reader :age, :phase, :ta
   attr_accessor :name
  def initialize(options = {})
-    @phase = 1
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
-  end
+  @phase = options.fetch(:phase,1)
+  @age = options.fetch(:age, 0)
+  @name = options.fetch(:name, "")
+ end
 
-   def offer_high_five
-    "High five!"
-  end
+ def offer_high_five
+  "High five!"
+ end
 end
 
