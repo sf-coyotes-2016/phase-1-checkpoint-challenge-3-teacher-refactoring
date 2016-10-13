3014f61660b1fd7e375feb6225698446a44312b5
Dana Cho <im.dana@gmail.com>
2016-10-13 10:23:45 -0700

    Checkpoint Challenge #3

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,51 +1,17 @@
-class ApprenticeTeacher
+require_relative 'SeniorTeacher'
+
+class ApprenticeTeacher < SeniorTeacher
   attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
   def initialize(options={})
-    @age = options.fetch(:age, 0)
+    @phase=3
+    TARGETRAISE = -200
     @name = options.fetch(:name, "")
-    @target_raise = 800
-    @phase = 3
+    @age = options.fetch(:age, 0)
+    RATING -10
   end
 
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
-
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works. "
-    response += "*drops crazy knowledge bomb* "
-    response += "... You're welcome."
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
 
   def attend_training_session
     puts "Whoa. I know ruby-fu"
