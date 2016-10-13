3492b594b0212016cfe9b53d3cf3ce0ec12589c6
jtsukano <jaytsukano@gmail.com>
2016-10-13 09:45:38 -0700

    create person class

---
+++ person.rb
@@ -0,0 +1,10 @@
+class Person
+	attr_reader :age, :phase
+	attr_accessor :name
+
+	def initialize(options={})
+		@age = options.fetch(:age, 0)
+		@name = options.fetch(:name, "")
+		@phase = self.class::PHASE
+	end
+end
