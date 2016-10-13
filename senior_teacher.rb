e5ced94e6bcc11fea628bf70f814571df2cde61b
Darren Lee & Aidan Legaspi <pair+dl804.legaspi21@devbootcamp.com>
2016-10-13 15:27:28 -0700

    Removes redundant attr methods

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,6 +1,6 @@
 require_relative 'educator'
 class SeniorTeacher < Educator
-  attr_reader :salary, :performance_rating, :target_raise
+  attr_reader :performance_rating
   RATING_THRESHOLD_FOR_RAISE = 90
 
   def initialize(options={})
@@ -13,18 +13,6 @@ class SeniorTeacher < Educator
     response += "Listen, class, this is how everything works, fo SHO! "
     response += "*drops flat-out insane knowledge bomb* "
     response += "... You're welcome. *saunters away*"
-    response
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
     response
   end
 
