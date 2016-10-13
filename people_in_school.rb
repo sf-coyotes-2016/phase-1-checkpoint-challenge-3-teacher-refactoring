d8b061042f7fb588aa532e1428fc38214ba9606a
CatonNip <vivinguyen000@gmail.com>
2016-10-13 09:50:01 -0700

    Create teachers.rb file and start puting redundet methods into a parent class for people in school

--- people_in_school.rb
+++ people_in_school.rb
@@ -1,1 +1,18 @@
-people_in_school.rb
+require_relative 'student'
+require_relative 'apprentice_teacher'
+require_relative 'senior_teacher'
+
+class PeopleInSchool
+	 attr_reader :age, :phase
+  attr_accessor :name
+ def initialize(options = {})
+    @phase = 1
+    @age = options.fetch(:age, 0)
+    @name = options.fetch(:name, "")
+  end
+
+   def offer_high_five
+    "High five!"
+  end
+end
+
---
+++ teachers.rb
@@ -0,0 +1,1 @@
+teachers.rb
