e4d6eefddfe1cb61dc4f6f045cba4c88c10bcccc
JessCodes <jessicafellison@gmail.com>
2016-10-18 19:00:54 -0700

    Delete comments

--- people_in_school.rb
+++ people_in_school.rb
@@ -3,11 +3,10 @@ require_relative 'greeting'
 class PeopleInSchool
   include Greeting
 
-  attr_reader :age, :phase
+  attr_reader :age, :phase, :salary
   attr_accessor :name
 
   def initialize(options = {})
-    # @phase = 1
     @age = options.fetch(:age, 0)
     @name = options.fetch(:name, "")
   end
--- senior_teacher.rb
+++ senior_teacher.rb
@@ -1,59 +1,17 @@
-# require_relative 'people_in_school'
 require_relative 'school_staff'
 
 class SeniorTeacher < Staff
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  # attr_accessor :name
+  attr_reader :phase, :target_raise
   RESPONSE2 = "Listen, class, this is how everything works, fo SHO! "
   RESPONSE3 = "*drops flat-out insane knowledge bomb* "
   RESPONSE4 = "... You're welcome. *saunters away*"
   RATING = 90
-  
+
   def initialize(options={})
     @phase = 3
-    # @age = options.fetch(:age, 0)
-    # @name = options.fetch(:name, "")
     @target_raise = 1000    
     super
   end
-
-  # def offer_high_five
-  #   "High five!"
-  # end
-
-  # def set_phase(num)
-  #   @phase = num
-  #   "Cool, I've always wanted to teach phase #{num}!"
-  # end
-
-  # def teach_stuff
-  #   response = ""
-  #   response += "Listen, class, this is how everything works, fo SHO! "
-  #   response += "*drops flat-out insane knowledge bomb* "
-  #   response += "... You're welcome. *saunters away*"
-  #   response
-  # end
-
-  # def salary=(new_salary)
-  #   puts "This better be good!"
-  #   @salary = new_salary
-  # end
-
-  # def receive_raise(raise)
-  #   @salary += raise
-  # end
-
-  # def set_performance_rating(rating)
-  #   response = ""
-  #   if rating > 90
-  #     response = "Yay, I'm a great employee!"
-  #     receive_raise(@target_raise)
-  #   else
-  #     response += "Oh, well -- thanks to this actionable, specific, and kind "
-  #     response += "feedback, I'll do better next time."
-  #   end
-  #   response
-  # end
 
   def lead_training_session
     puts "Hey newbie!  Here are some common pitfalls.  Don't fall in them!"
